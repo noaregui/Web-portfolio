9d60ed23b34b3500714e4b8ff081d84ea44e5e60
Ainhoa <ainhoaleonarrieta@gmail.com>
2026-01-15 15:19:21 +0100

    Eliminando altura fija de main.content

--- src/App.vue
+++ src/App.vue
@@ -51,9 +51,8 @@ body {
   display: flex;
   flex-direction: column;
   width: 100%;
-  min-height: 100vh;
+  /* min-height: 100vh; */
   box-sizing: border-box;
-  justify-content: center;
 }
 
 .main-content .container {
